a113cc14e3afce4fdf2c53c7e8b79e0571616a71
sraghudatta94 <sraghudatta94@gmail.com>
2016-11-27 00:01:52 +0530

    Added Carousel,status bar,scrollspy, popover, menu bar, glyph icons, tabs, maps but facing issues in glyph icons

---
+++ js/script.js
@@ -0,0 +1,23 @@
+$(function() {
+	
+	$('#alertMe').click(function(e) {
+		
+		e.preventDefault();
+		
+		$('#successAlert').slideDown();
+		
+	});
+	
+	$('a.pop').click(function(e) {
+		e.preventDefault();
+	});
+	
+	$('a.pop').popover();
+	
+	$('[rel="tooltip"]').tooltip();
+	
+	
+});
+$(document).ready(function(){
+    $('[data-toggle="popover"]').popover();   
+});
